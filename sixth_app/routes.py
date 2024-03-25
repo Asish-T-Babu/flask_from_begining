a30b43d6c9383051c93f19599c2116016ab7bae0
Asish-T-Babu <asishtbabu@gmail.com>
2024-03-26 00:58:25 +0530

    updated codes

--- sixth_app/routes.py
+++ sixth_app/routes.py
@@ -1,10 +1,31 @@
-from flask import render_template, request
+from flask import render_template, request, jsonify
 
 from models import Person
 
 def register_routes(app, db):
     
-    @app.route('/')
+    @app.route('/', methods=['GET','POST'])
     def index():
-        people = Person.query.all()
-        return str(people)
+        if request.method == 'POST':
+            name = request.form.get('name')
+            age = int(request.form.get('age'))
+            job = request.form.get('job')
+            person = Person(name=name, age=age, job=job)
+            db.session.add(person)
+            db.session.commit()
+            people = Person.query.all()
+            return render_template('index.html', people = people)
+        if request.method == "GET":
+            people = Person.query.all()
+            return render_template('index.html', people = people)
+
+    @app.route('/delete/<pid>', methods=['DELETE'])
+    def delete(pid):
+        Person.query.filter(Person.pid == pid).delete()
+        db.session.commit()
+        return jsonify({'status': 'success', 'data':'Person deleted successfully'}), 200
+
+    @app.route('/details/<pid>')
+    def details(pid):
+        person = Person.query.filter(Person.pid == pid).first()
+        return render_template('detail.html', person= person)
