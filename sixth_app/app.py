ceb92ed92a6a941aa41e0708bed264ce368a3647
Asish-T-Babu <asishtbabu@gmail.com>
2024-03-27 00:13:21 +0530

    sixt project

--- sixth_app/app.py
+++ sixth_app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 
@@ -8,10 +10,22 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./testdb.db'
 
+    app.secret_key = 'SOME KEY'
+
     db.init_app(app)
 
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+
+    from models import Users
+
+    @login_manager.user_loader
+    def load_user(uid):
+        return Users.query.get(uid)
+    
+    bcrypt = Bcrypt(app)
     from routes import register_routes
-    register_routes(app, db)
+    register_routes(app, db, bcrypt)
 
     migrate= Migrate(app, db)
     return app
